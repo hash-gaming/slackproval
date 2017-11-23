d05b5fa0cf010b475fec1416f95762f18664602f
Michael Stephens <michael@stephens.tech>
2017-11-23 09:16:24 -0800

    Error styling for forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def errors_for(object)
+    if object.errors.any?
+      content_tag(:div, class: "card border-danger") do
+        concat(content_tag(:div, class: "card-header bg-danger text-white") do
+          concat "#{pluralize(object.errors.count, "error")} prohibited this #{object.class.name.downcase} from being saved:"
+        end)
+        concat(content_tag(:div, class: "card-body") do
+          concat(content_tag(:ul, class: 'mb-0') do
+            object.errors.full_messages.each do |msg|
+              concat content_tag(:li, msg)
+            end
+          end)
+        end)
+      end
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: true
 
   def admin?
     admin == true
