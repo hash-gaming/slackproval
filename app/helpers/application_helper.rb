33bc807a218f8648af0961e14d9e89328dc8c7ac
Michael Stephens <michael@stephens.tech>
2017-11-25 17:20:50 -0800

    Working request form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
   def errors_for(object)
     if object.errors.any?
-      content_tag(:div, class: "card border-danger") do
-        concat(content_tag(:div, class: "card-header bg-danger text-white") do
+      content_tag(:article, class: "message is-danger") do
+        concat(content_tag(:div, class: "message-header") do
           concat "#{pluralize(object.errors.count, "error")} prohibited this #{object.class.name.downcase} from being saved:"
         end)
-        concat(content_tag(:div, class: "card-body") do
+        concat(content_tag(:div, class: "message-body") do
           concat(content_tag(:ul, class: 'mb-0') do
             object.errors.full_messages.each do |msg|
               concat content_tag(:li, msg)
